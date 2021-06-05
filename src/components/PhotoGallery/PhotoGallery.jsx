b54e46f2c1b35171c4f6eb5f5211d68233fb165a
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-05 16:40:06 -0500

    Passed props to PhotoGallery

--- src/components/PhotoGallery/PhotoGallery.jsx
+++ src/components/PhotoGallery/PhotoGallery.jsx
@@ -1,11 +1,14 @@
 import PhotoGalleryItem from "../PhotoGalleryItem/PhotoGalleryItem.jsx"
 
 function PhotoGallery ({list}) {
-
+    console.log('props inside list', list);
     return(
         <div>
             <h3>Gallery goes here:</h3>
-           
+            {list.map(galleryItems => 
+                    <PhotoGalleryItem key={galleryItems.id} id={galleryItems.id} path={galleryItems.path} description={galleryItems.description}/>
+                )
+            }
         </div>
     )
 }
