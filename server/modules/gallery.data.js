6d26d7a8980d84910403ee5848c7bd78eb3302a4
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-05 11:44:49 -0500

    Set Up Files

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,8 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/Image-One.jpeg', description: 'Sometime the best trips start at two in the morning. That way, you get to see the sunrise!', likes: 0 };
-    { id: 2, path: 'images/Image-Two.jpeg', description: 'The Superior Hiking Trail on the North Shore is one of the most gorgeous places on this planet, let alone Minnesota', likes: 0 };
-    { id: 3, path: 'images/Image-Three.jpeg', description: 'Glacier National Park is the most beautiful National State Park. Convince me otherwise.', likes: 0 };
-    { id: 4, path: 'images/Image-Four.jpeg', description: 'Hiking on the coast is more foggy than not', likes: 0 }
+    { id: 1, path: 'images/Image-One.jpeg', description: 'Sometime the best trips start at two in the morning. That way, you get to see the sunrise!', likes: 0 },
+    { id: 2, path: 'images/Image-Two.jpeg', description: 'The Superior Hiking Trail on the North Shore is one of the most gorgeous places on this planet, let alone Minnesota', likes: 0 },
+    { id: 3, path: 'images/Image-Three.jpeg', description: 'Glacier National Park is the most beautiful National State Park. Convince me otherwise.', likes: 0 },
+    { id: 4, path: 'images/Image-Four.jpeg', description: 'Hiking on the coast is more foggy than not', likes: 0 },
     { id: 5, path: 'images/Image-Five.jpeg', description: 'Have you climbed a mountain? I have!', likes: 0 }
 ];
 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  
+    const [photoGallery, setPhotoGallery] = useState([]);
+
+    // ⬇ When the page is loaded, call getPhotos function
+    useEffect(() => {
+      getPhotos()
+    }, []);
+
+    const getPhotos = () => {
+      console.log('in getPhotos')
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/Image-Five.jpeg" width="200px" height="200px"/>
       </div>
     );
 }
