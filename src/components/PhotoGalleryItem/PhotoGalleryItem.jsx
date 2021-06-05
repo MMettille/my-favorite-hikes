02aa7eca37d5d92e30fb28901cab61c464b132af
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-05 18:02:05 -0500

    Conditional Rendering Setup in PhotoGalleryItem

--- src/components/PhotoGalleryItem/PhotoGalleryItem.jsx
+++ src/components/PhotoGalleryItem/PhotoGalleryItem.jsx
@@ -3,12 +3,24 @@ import {useState} from 'react';
 import axios from 'axios';
 
 function PhotoGalleryItem ({galleryItems, getPhotoGallery}) {
+    
+    const [isHidden, setIsHidden] = useState(false);
 
+    const imageClick = () => {
+        setIsHidden(!isHidden);
+        console.log(isHidden);
+    }
     return (
         <>
+            { isHidden ? (
+                
+            )}
             {/* // ⬇ Loops through and renders each photo */}
             <div className="photo">
-                <img src={galleryItems.path} width="200px" height="200px" />
+                <img src={galleryItems.path} width="200px" height="200px"  onClick={() => setIsHidden(!isHidden)} />
+                {/* <button onClick={() => setIsHidden(!isHidden)}>
+                    {!isHidden ? 'Hide' : 'Show'}
+                </button> */}
             </div>
         </>
     )
