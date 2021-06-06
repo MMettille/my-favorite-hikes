28dc87842a893194870c2f0e5a5be27dbc43ecaa
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-06 15:48:51 -0500

    Base Mode Done

--- src/components/PhotoGallery/PhotoGallery.jsx
+++ src/components/PhotoGallery/PhotoGallery.jsx
@@ -5,7 +5,8 @@ function PhotoGallery ({list, getPhotoGallery}) {
     console.log('props inside list', list);
     return(
         <div>
-            <h3>My Favorite Hikes:</h3>
+            <h3>My Favorite Hikes:</h3><br />
+            <p>I am an outdoorsy person and camp, backpack, and/or climb whenever I get the chance. Click the images below to learn where I've been and don't forget to vote for your favorite!</p>
                 <div className="photo-gallery">
                     {list.map(galleryItems => 
                         // ⬇ Each photo gets send into the PhotoGalleryItem
