8969bd78159e678687b36787b51942addf8473ce
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-13 12:19:02 -0500

    Changed the Header Text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import"./Header.css";
 function Header(){
     return (
         <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
+          <h1 className="App-title">My Favorite Hikes:</h1>
         </header>
     )
 }
